39a9ed3877ae8780a1bed5dd6e930108c591f2be
AlekseyMegliy <meglij46@gmail.com>
2023-04-19 14:16:20 +0300

    deploy fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,11 @@ import type { AppProps } from "next/app";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import { useEffect, useState } from "react";
-function useWindowSize() {
-  const [size, setSize] = useState([1000]);
-  useEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth]);
-    }
-    window.addEventListener("resize", updateSize);
-    updateSize();
-    return () => window.removeEventListener("resize", updateSize);
-  }, []);
-  return size;
-}
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [width] = useWindowSize();
-
   return (
     <>
-      <Header width={width} />
+      <Header />
       <Component {...pageProps} />
       <Footer />
     </>
